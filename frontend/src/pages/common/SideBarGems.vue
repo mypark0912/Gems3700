c914f326ea6fd851ab1ae83415a22a8990d23d88
ntek-pengsoo <77426824+ntek-pengsoo@users.noreply.github.com>
2025-09-03 17:49:26 +0900

    사이드바 수정부분 재반영

--- frontend/src/pages/common/SideBarGems.vue
+++ frontend/src/pages/common/SideBarGems.vue
@@ -94,7 +94,7 @@
                     <router-link to="/diagnosis/Main" custom v-slot="{ href, navigate, isExactActive }">
                       <li class="mb-1 last:mb-0">
                         <a class="block transition truncate" :class="isExactActive ? 'text-violet-500' : 'text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200'" :href="href" @click="navigate">
-                          <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Power Quality Diagnosis</span>
+                          <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">{{ t("sidebar.powerQuality") }}</span>
                         </a>
                       </li>
                     </router-link>                                
@@ -109,7 +109,7 @@
                     <svg class="shrink-0 fill-current" :class="isSubActive ? 'text-violet-500' : 'text-gray-400 dark:text-gray-500'" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
                         <path d="M12 1a1 1 0 1 0-2 0v2a3 3 0 0 0 3 3h2a1 1 0 1 0 0-2h-2a1 1 0 0 1-1-1V1ZM1 10a1 1 0 1 0 0 2h2a1 1 0 0 1 1 1v2a1 1 0 1 0 2 0v-2a3 3 0 0 0-3-3H1ZM5 0a1 1 0 0 1 1 1v2a3 3 0 0 1-3 3H1a1 1 0 0 1 0-2h2a1 1 0 0 0 1-1V1a1 1 0 0 1 1-1ZM12 13a1 1 0 0 1 1-1h2a1 1 0 1 0 0-2h-2a3 3 0 0 0-3 3v2a1 1 0 1 0 2 0v-2Z" />
                       </svg>
-                    <span class="text-sm font-medium ml-4 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Sub Channel</span>
+                    <span class="text-sm font-medium ml-4 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">{{ t("sidebar.sub") }}</span>
                   </div>
                 </a>
               </li>
@@ -482,7 +482,16 @@
             }else{
               return false;
             }
-          }else{
+          }else if (currentRoute.fullPath.includes("/diagnosis")) {
+        // ✅ 추가
+        if (currentRoute.params.channel === "Main" || propdata === "Main") {
+          return true;
+        } else {
+          return false;
+        }
+      } 
+          
+          else{
             return false;
           }
        }); // ✅ Vuex getters 변경
